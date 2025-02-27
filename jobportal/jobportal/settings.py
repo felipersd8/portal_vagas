ac96e74793389441064f8f026b14422a2e7c9d00
felipersd8 <seu-email@exemplo.com>
2025-02-26 21:36:32 -0300

    Configuração inicial do projeto Job Portal

---
+++ jobportal/jobportal/settings.py
@@ -0,0 +1,13 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'jobs',
+]
+
+# 2. Configuração do banco de dados (geralmente SQLite por padrão)
+# 3. Configurações de idioma e fuso horário
+# 4. Configurações de arquivos estáticos 
